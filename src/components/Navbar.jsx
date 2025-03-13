3a7e09a97f31597d79d358888985d732438a1b0e
Sahaj Rajput <sahajrajput1112@gmail.com>
2025-03-13 22:50:55 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <div className='d-flex  justify-content-center'>
     <div className="navbar d-flex bg-white mt-4 justify-content-center hcc rounded-lg" style={{ boxShadow: '0px 4px 12px rgba(255, 255, 255, 0.5), 0px 0px 20px rgba(0, 123, 255, 0.2)' }}>
        
-       <div className="nav-links-container fs-5 gap-3 gap-lg-5  d-flex">
+       <div className="nav-links-container fs-5 gap-4 gap-lg-5  d-flex">
          <Link to="/" className="nav-link">Home</Link>
          <Link to="/about" className="nav-link">About</Link> 
          <Link to="/projects" className="nav-link">Projects</Link>
